fb9b3fe1b63481f987717b0b594a8cf207adb056
Matt Poole <matt.poole@bixal.com>
2024-04-09 17:14:35 -0400

    Switch to a robo task to initialize the content needed for scaffolding to occur.

--- src/Robo/Plugin/Commands/DrupalEnvCommands.php
+++ src/Robo/Plugin/Commands/DrupalEnvCommands.php
@@ -13,11 +13,11 @@ class DrupalEnvCommands extends Tasks
 {
 
   /**
-   * Do tasks that are required before the scaffolding can be applied.
+   * Run the scaffolding and bring in new / updated files.
    *
-   * @command drupal-env:init
+   * @command drupal-env:scaffold
    */
-  public function dplInit() {
+  public function drupalEnvScaffold() {
     $composer_path = 'composer';
     if (!`which $composer_path`) {
       if (!`which docker`) {
@@ -25,23 +25,23 @@ class DrupalEnvCommands extends Tasks
       }
       $composer_path = 'docker run --rm -i --tty -v $PWD:/app composer:2';
     }
-    $composer_json = json_decode(file_get_contents('composer.json'), true);
+
     // Make sure that our scaffolding can run.
-    if (empty($composer_json['extra']['drupal-scaffold']['allowed-packages']) || !in_array('mpbixal/drupal-env', $composer_json['extra']['drupal-scaffold']['allowed-packages'] ?? [])) {
-      $this->_exec($composer_path . ' config extra.drupal-scaffold.allowed-packages --json --merge \'["mpbixal/drupal-env"]\'');
-    }
-    $web_root = $composer_json['extra']['drupal-scaffold']['locations']['web-root'] ?? 'web';
-    $web_root = rtrim($web_root, '/');
+    $this->enableScaffolding();
+
     // Ensure that settings.php is in place so it can be appended to by the
     // scaffolding.
+    $web_root = $composer_json['extra']['drupal-scaffold']['locations']['web-root'] ?? 'web';
+    $web_root = rtrim($web_root, '/');
     if (!file_exists("$web_root/sites/default/settings.php") && file_exists("$web_root/sites/default/default.settings.php")) {
       $this->_copy("$web_root/sites/default/default.settings.php", "$web_root/sites/default/settings.php");
     }
+
     // Add autoloading so that the robo tasks that are scaffolded in will work.
+    $composer_json = $this->getComposerJson();
     if (empty($composer_json['autoload']['psr-4']) || !in_array('./RoboEnv/', $composer_json['autoload']['psr-4'] ?? [])) {
       $composer_json['autoload']['psr-4']['RoboEnv\\'] = './RoboEnv/';
-      // composer config does not support 'autoload'.
-      file_put_contents('composer.json', json_encode($composer_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . "\n");
+      $this->saveComposerJson($composer_json);
     }
 
     // Create the config sync directory.
@@ -56,6 +56,59 @@ class DrupalEnvCommands extends Tasks
 
     // Now that everything is ready, run the scaffolding.
     $this->_exec($composer_path . ' drupal:scaffold');
+
+    $this->disableScaffolding();
+    $this->yell('The scaffolding has been updated and disable from running until this is called again.');
   }
 
+  /**
+   * Retrieve the value of composer.json.
+   *
+   * @return array
+   */
+  protected function getComposerJson(): array
+  {
+    return json_decode(file_get_contents('composer.json'), true);
+  }
+
+  /**
+   * Save composer.json.
+   *
+   * @param array $composer_json
+   *
+   * @return void
+   */
+  protected function saveComposerJson(array $composer_json): void
+  {
+    file_put_contents('composer.json', json_encode($composer_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . "\n");
+  }
+
+  /**
+   * Turn on scaffolding in composer.json.
+   *
+   * @return void
+   */
+  protected function enableScaffolding(): void
+  {
+    $composer_json = $this->getComposerJson();
+    if (empty($composer_json['extra']['drupal-scaffold']['allowed-packages']) || !in_array('mpbixal/drupal-env', $composer_json['extra']['drupal-scaffold']['allowed-packages'] ?? [])) {
+      $composer_json['extra']['drupal-scaffold']['allowed-packages'] = ['mpbixal/drupal-env'];
+      $this->saveComposerJson($composer_json);
+      //$this->_exec($composer_path . ' config extra.drupal-scaffold.allowed-packages --json --merge \'["mpbixal/drupal-env"]\'');
+    }
+  }
+
+  /**
+   * Turn off scaffolding in composer.json.
+   *
+   * @return void
+   */
+  protected function disableScaffolding(): void
+  {
+    $composer_json = $this->getComposerJson();
+    if (!empty($composer_json['extra']['drupal-scaffold']['allowed-packages']) && false !== $key = array_search('mpbixal/drupal-env', $composer_json['extra']['drupal-scaffold']['allowed-packages'] ?? [])) {
+      unset($composer_json['extra']['drupal-scaffold']['allowed-packages'][$key]);
+      $this->saveComposerJson($composer_json);
+    }
+  }
 }
