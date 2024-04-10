c2d1ee7051cd39faf355da94edf79fc20e890d12
Matt Poole <matt.poole@bixal.com>
2024-04-10 15:52:00 -0400

    Use an abstract class for drupal env commands so it can be re-used.

--- src/Robo/Plugin/Commands/DrupalEnvCommands.php
+++ src/Robo/Plugin/Commands/DrupalEnvCommands.php
@@ -9,25 +9,29 @@ use Robo\Tasks;
  *
  * @class RoboFile
  */
-class DrupalEnvCommands extends Tasks
+class DrupalEnvCommands extends DrupalEnvCommandsBase
 {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected string $package_name = 'mpbixal/drupal-env';
 
   /**
    * Update the environment so that the scaffolding can happen, and run it.
    *
    * @command drupal-env:scaffold
    */
-  public function drupalEnvScaffold(): void {
-    $composer_path = 'composer';
-    if (!`which $composer_path`) {
-      if (!`which docker`) {
-        throw new \Exception('Either composer or docker must be installed to continue');
-      }
-      $composer_path = 'docker run --rm -i --tty -v $PWD:/app composer:2';
-    }
+  public function drupalEnvScaffold(): void
+  {
+    $this->updateScaffolding();
+  }
 
-    $composer_json_hash_before = md5_file('composer.json');
-
+  /**
+   * {@inheritdoc}
+   */
+  protected function preScaffoldChanges(): void
+  {
     $composer_json = $this->getComposerJson();
 
     // Ensure that settings.php is in place, so it can be appended to by the
@@ -75,85 +79,6 @@ class DrupalEnvCommands extends Tasks
       $composer_json['scripts']['post-drupal-scaffold-cmd'][] = $post_drupal_scaffold_cmd;
       $this->saveComposerJson($composer_json);
     }
-
-    // Make sure that our scaffolding can run.
-    $this->enableScaffolding();
-
-    // Now that everything is ready, run the scaffolding.
-    $this->_exec($composer_path . ' drupal:scaffold');
-
-    $this->disableScaffolding();
-
-    // If composer.json was updated, the lock file also has to be updated.
-    if ($composer_json_hash_before !== md5_file('composer.json')) {
-      $this->_exec($composer_path . ' update --lock');
-    }
-
-    $this->yell('The scaffolding has been enabled and run, bringing in any new scaffolded files. Afterwords, it was disabled so that scaffolding is not updated every time composer install is called.');
-  }
-
-  /**
-   * Retrieve the value of composer.json.
-   *
-   * @return array
-   */
-  protected function getComposerJson(): array
-  {
-    return json_decode(file_get_contents('composer.json'), true);
-  }
-
-  /**
-   * Save composer.json.
-   *
-   * @param array $composer_json
-   *
-   * @return void
-   */
-  protected function saveComposerJson(array $composer_json): void
-  {
-    file_put_contents('composer.json', json_encode($composer_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . "\n");
-  }
-
-  /**
-   * Turn on scaffolding in composer.json for a single $project.
-   *
-   * @param string $project
-   *   A project that has scaffolding.
-   *
-   * @return bool
-   *   True if composer.json needed to be updated.
-   */
-  protected function enableScaffolding(string $project = 'mpbixal/drupal-env'): bool
-  {
-    $composer_json = $this->getComposerJson();
-    if (!in_array($project, $composer_json['extra']['drupal-scaffold']['allowed-packages'] ?? [])) {
-      $composer_json['extra']['drupal-scaffold']['allowed-packages'][] = $project;
-      $this->saveComposerJson($composer_json);
-      //$this->_exec($composer_path . ' config extra.drupal-scaffold.allowed-packages --json --merge \'["mpbixal/drupal-env"]\'');
-      return true;
-    }
-    return false;
-  }
-
-  /**
-   * Turn off scaffolding in composer.json for a single $project.
-   *
-   * @param string $project
-   *   A project that has scaffolding.
-   *
-   * @return bool
-   *   True if composer.json needed to be updated.
-   *
-   */
-  protected function disableScaffolding(string $project = 'mpbixal/drupal-env'): bool
-  {
-    $composer_json = $this->getComposerJson();
-    if (false !== $key = array_search($project, $composer_json['extra']['drupal-scaffold']['allowed-packages'] ?? [])) {
-      unset($composer_json['extra']['drupal-scaffold']['allowed-packages'][$key]);
-      $this->saveComposerJson($composer_json);
-      return true;
-    }
-    return false;
   }
 
 }
---
+++ src/Robo/Plugin/Commands/DrupalEnvCommandsBase.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace DrupalEnv\Robo\Plugin\Commands;
+
+use Robo\Tasks;
+
+/**
+ * Provide commands to handle installation tasks.
+ *
+ * @class RoboFile
+ */
+abstract class DrupalEnvCommandsBase extends Tasks {
+
+  /**
+   * The name of this package.
+   *
+   * @var string
+   */
+  protected string $package_name;
+
+  /**
+   * Retrieve the package name.
+   *
+   * @return string
+   *
+   * @throws \Exception
+   */
+  protected function getPackageName(): string
+  {
+    if (empty($this->package_name)) {
+      throw new \Exception('$this->package_name must be set.');
+    }
+    return $this->package_name;
+  }
+
+  /**
+   * Get the path to composer.
+   *
+   * @return string
+   *
+   * @throws \Exception
+   */
+  protected function getComposerPath(): string
+  {
+    if (`which ./composer`) {
+      return './composer';
+    } elseif (`which composer`) {
+      return 'composer';
+    } elseif (`which docker`) {
+      return 'docker run --rm -i --tty -v $PWD:/app composer:2';
+    }
+    throw new \Exception('Either composer or docker must be installed to continue');
+  }
+
+  /**
+   * Update all the scaffolding in the current project.
+   *
+   * @return void
+   *
+   * @throws \Exception
+   */
+  protected function updateScaffolding(): void
+  {
+    $composer_path = $this->getComposerPath();
+
+    $composer_json_hash_before = md5_file('composer.json');
+
+    // Take care of any items needed before scaffolding happens.
+    $this->preScaffoldChanges();
+
+    // Make sure that our scaffolding can run.
+    $this->enableScaffolding();
+
+    // Now that everything is ready, run the scaffolding.
+
+    $this->_exec($composer_path . ' drupal:scaffold');
+
+    $this->disableScaffolding();
+
+    // If composer.json was updated, the lock file also has to be updated.
+    if ($composer_json_hash_before !== md5_file('composer.json')) {
+      $this->_exec($composer_path . ' update --lock');
+    }
+
+    $this->yell('The scaffolding has been enabled and run, bringing in any new scaffolded files. Afterwords, it was disabled so that scaffolding is not updated every time composer install is called.');
+  }
+
+  /**
+   * Take care of any items needed before scaffolding happens.
+   *
+   * @return void
+   */
+  abstract protected function preScaffoldChanges(): void;
+
+  /**
+   * Retrieve the value of composer.json.
+   *
+   * @return array
+   */
+  protected function getComposerJson(): array
+  {
+    return json_decode(file_get_contents('composer.json'), true);
+  }
+
+  /**
+   * Save composer.json.
+   *
+   * @param array $composer_json
+   *
+   * @return void
+   */
+  protected function saveComposerJson(array $composer_json): void
+  {
+    file_put_contents('composer.json', json_encode($composer_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . "\n");
+  }
+
+  /**
+   * Turn on scaffolding in composer.json for a single $project.
+   *
+   * @return bool
+   *   True if composer.json needed to be updated.
+   */
+  protected function enableScaffolding(): bool
+  {
+    $composer_json = $this->getComposerJson();
+    if (!in_array($this->package_name, $composer_json['extra']['drupal-scaffold']['allowed-packages'] ?? [])) {
+      $composer_json['extra']['drupal-scaffold']['allowed-packages'][] = $this->package_name;
+      $this->saveComposerJson($composer_json);
+      //$this->_exec($composer_path . ' config extra.drupal-scaffold.allowed-packages --json --merge \'["mpbixal/drupal-env"]\'');
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Turn off scaffolding in composer.json for a single $project.
+   *
+   * @return bool
+   *   True if composer.json needed to be updated.
+   *
+   */
+  protected function disableScaffolding(): bool
+  {
+    $composer_json = $this->getComposerJson();
+    if (false !== $key = array_search($this->package_name, $composer_json['extra']['drupal-scaffold']['allowed-packages'] ?? [])) {
+      unset($composer_json['extra']['drupal-scaffold']['allowed-packages'][$key]);
+      $this->saveComposerJson($composer_json);
+      return true;
+    }
+    return false;
+  }
+
+}
