6136bfd7cb9be932f20ee0fe244f8d60ec389fb5
Matt Poole <matt.poole@bixal.com>
2024-06-14 17:06:11 -0400

    Allow installation profile to be a choice on installation.

--- scaffolding/orch/deploy_install.sh
+++ scaffolding/orch/deploy_install.sh
@@ -19,13 +19,30 @@ if [ -n "$(drush status | grep pgsql 2>/dev/null)" ]; then
   drush sql:query 'CREATE EXTENSION IF NOT EXISTS pg_trgm;'
 fi
 
+# Prompts the user for installation profile to use.
+drupal_profile() {
+  # If on the remote, don't prompt, just use minimal. This should never be hit because they should have exported config
+  # before running on the remote...but you never know.
+  if [ -z "$DRUPAL_ENV_REMOTE" ]; then
+    # Prompt the user for a string value
+    read -p "Which installation profile would you like to install? 'standard' and 'minimal' are available in core, you must install others yourself before continuing. Press enter for 'minimal'.): " profile
+  fi
+
+  # Default to 'minimal' if no value is provided
+  if [ -z "$profile" ]; then
+    profile="minimal"
+  fi
+
+  echo "$profile"
+}
+
 if [ -n "$(ls $(drush php:eval "echo realpath(Drupal\Core\Site\Settings::get('config_sync_directory'));")/*.yml 2>/dev/null)" ]; then
   # Find the profile in config.
   PROFILE=$(grep 'profile:' config/sync/core.extension.yml 2>/dev/null | awk '{print $2}')
   # Check if 'grep' found a match
   if [ -z "$PROFILE" ]; then
       # Set default value to 'minimal'
-      PROFILE="minimal"
+      PROFILE=$(drupal_profile)
   fi
   echo "Installing a fresh Drupal site from configuration"
   drush si -y --account-pass='admin' --existing-config ${PROFILE}
@@ -36,7 +53,7 @@ if [ -n "$(ls $(drush php:eval "echo realpath(Drupal\Core\Site\Settings::get('co
   fi
 else
   echo "Installing a fresh Drupal site without configuration"
-  drush si -y --account-pass='admin' minimal
+  drush si -y --account-pass='admin' $(drupal_profile)
 fi
 
 # Clear cache after installation
